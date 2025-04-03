1d7b2d5edaa806e42a749160a145b82915e80923
McShaman <timmclaren@mcshaman.com>
2025-04-04 07:31:45 +1000

    Still not working

--- Test iOS project/Test_iOS_projectApp.swift
+++ Test iOS project/Test_iOS_projectApp.swift
@@ -7,6 +7,9 @@ import SwiftUI
 
 @main
 struct Test_iOS_projectApp: App {
+    init() {
+        LynxEnv.sharedInstance()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ Test iOS project/Test_iOS_project_Bridging_Header.h
@@ -0,0 +1,16 @@
+//
+//  Test iOS project Bridging Header.h
+//  Test iOS project
+//
+//  Created by Tim McLaren on 30/3/2025.
+//
+
+#ifndef Test_iOS_project_Bridging_Header_h
+#define Test_iOS_project_Bridging_Header_h
+
+#import <Lynx/LynxConfig.h>
+#import <Lynx/LynxEnv.h>
+#import <Lynx/LynxTemplateProvider.h>
+#import <Lynx/LynxView.h>
+
+#endif /* Test_iOS_project_Bridging_Header_h */
